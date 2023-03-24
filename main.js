56d0d26e255ff18019edd44e1f9a7227f21235f2
Aziza <kantemir@MacBook-Air-Aziza.local>
2023-03-24 11:19:36 +0600

    Initial HW folder

---
+++ main.js
@@ -0,0 +1,53 @@
+//! Задача 1
+// let admin;
+// let name = 'Джон';
+// admin = name;
+// console.log(admin);
+
+
+//! Задача 2
+
+// let str = "Privet, Mir!";
+// console.log(str)
+
+// let str1 = "Privet, ";
+// let str2 = "Mir!";
+// console.log(str1 + str2);
+
+// let name = "Arstan";
+// console.log("Privet, " + name + "!")
+
+// let age = 17;
+// console.log("Mne " + age + " let!")
+
+
+//! Задача 3
+
+// let num = 3;
+// console.log(num)
+
+// let a = 10;
+// let b = 2;
+// console.log(a + b) // Summa
+// console.log(a - b) // Raznost
+// console.log(a * b) // Proizvedenie
+// console.log(a / b) // Delenie
+
+// let c = 15;
+// let d = 2;
+// let result = c + b;
+// console.log(result)
+
+
+// let a = 10; 
+// let b = 2; 
+// let c = 5;
+// console.log(a + b + c)
+
+
+let a = 17;
+let b = 10;
+let c = a + b;
+let d = 7;
+let result = c + d;
+console.log(result)
